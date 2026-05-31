2d7a29d91c74c1c4df891a9c96058576583ad9f1
EducateMuhdAdam <educatemuhdadam2004@gmail.com>
2026-06-01 06:28:30 +0800

    Added Building Logic

---
+++ Scripts/building.gd
@@ -0,0 +1,8 @@
+extends StaticBody2D
+class_name Building
+
+@export var x_size: int = 2
+@export var y_size: int = 1
+
+var interact: Callable = func():
+	pass
---
+++ Scripts/building_handler.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@export var default: Resource = preload("res://Scenes/farm_lv1.tscn")
+
+var guide: Building = null
+var is_build_mode: bool = true
+var anchor: Vector2 = Vector2(0,0)
+var building_list: Array[Building] = []
+const tilesize: int = 32
+
+func _ready() -> void:
+	setup_guide(default)
+	
+
+func _process(delta: float) -> void:
+	if is_build_mode:
+		guide.global_position = (get_global_mouse_position() / tilesize).snapped(Vector2.ONE) * tilesize
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			create_building(default)
+
+func setup_guide(building: Resource) -> void:
+	guide = building.instantiate()
+	add_child(guide)
+	
+func create_building(building: Resource) -> void:
+	var new = building.instantiate()
+	new.global_position = guide.global_position
+	building_list.append(new)
+	add_child(new)
+	
